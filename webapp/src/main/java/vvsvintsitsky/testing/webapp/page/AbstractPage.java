fbae6e2cd5f1db816bfa2849701d023f459a89f1
jetalai3 <jetalai3@gmail.com>
2016-06-02 23:20:27 +0200

    added partial localization

--- webapp/src/main/java/vvsvintsitsky/testing/webapp/page/AbstractPage.java
+++ webapp/src/main/java/vvsvintsitsky/testing/webapp/page/AbstractPage.java
@@ -1,30 +1,43 @@
 package vvsvintsitsky.testing.webapp.page;
 
+import java.util.Calendar;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
+import vvsvintsitsky.testing.webapp.component.localization.LanguageSelectionComponent;
 import vvsvintsitsky.testing.webapp.component.menu.MenuPanel;
 import vvsvintsitsky.testing.webapp.component.menu.MenuPanelLoggedUser;
 import vvsvintsitsky.testing.webapp.page.account.AccountsPage;
 
 public abstract class AbstractPage extends WebPage {
 
-    public AbstractPage() {
-        super();
-    }
+	private static final long serialVersionUID = -1790690697853654508L;
 
-    public AbstractPage(PageParameters parameters) {
-        super(parameters);
-    }
+	public AbstractPage() {
+		super();
+	}
 
-    @Override
-    protected void onInitialize() {
-        super.onInitialize();
+	public AbstractPage(PageParameters parameters) {
+		super(parameters);
+	}
 
-        
-            add(new MenuPanel("menu-panel"));
-        
+	@Override
+	protected void onInitialize() {
+		super.onInitialize();
 
-    }
+		add(new LanguageSelectionComponent("language-select"));
+
+		add(new MenuPanel("menu-panel"));
+
+//		WebMarkupContainer footer = new WebMarkupContainer("footer");
+//		add(footer);
+//		footer.add(AttributeModifier.append("onclick", "alert('Im clicked')"));
+
+	}
 
 }
